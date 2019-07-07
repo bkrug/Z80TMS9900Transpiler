6728492f490a530353e2143be9cc96281f112dc6
Benjamin Krug <benjaminkrug@yahoo.com>
2019-07-07 16:13:13 -0400

    Create new console project

---
+++ Z80toTMS9900console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z80toTMS9900console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

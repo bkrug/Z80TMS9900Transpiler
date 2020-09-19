abfe75062922a1255b6b67275ee22a718e74d71e
Benjamin Krug <benjaminkrug@yahoo.com>
2020-09-19 17:33:09 -0400

    Accept more command line arguements

--- Z80toTMS9900console/Program.cs
+++ Z80toTMS9900console/Program.cs
@@ -11,21 +11,24 @@ namespace Z80toTMS9900console
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length < 2 || args.Length > 4)
                 throw new Exception("Required arguments are a source file of z80 code and a destination file of TMS9900 code.");
             var z80AssemblyFile = args[0];
             var tms9900AssemblyFile = args[1];
+            var hexPrefix = args.Length > 2 ? args[2] : string.Empty;
+            var hexSuffix = args.Length > 3 ? args[3] : "h";
+
             using (var reader = new StreamReader(z80AssemblyFile))
             using (var writer = new StreamWriter(tms9900AssemblyFile))
             {
-                var translator = InstantiateTranslator();
+                var translator = InstantiateTranslator(hexPrefix, hexSuffix);
                 var z80codeLines = GetZ80sourceCode(reader);
                 foreach (var tmsCommand in translator.Translate(z80codeLines))
                     writer.WriteLine(tmsCommand.CommandText);
             }
         }
 
-        private static TMS9900MultilineTranslator InstantiateTranslator()
+        private static TMS9900MultilineTranslator InstantiateTranslator(string hexPrefix, string hexSuffix)
         {
             var singleLineTranslator = new TMS9900Translator(new List<(Z80SourceRegister, WorkspaceRegister)>()
                 {
@@ -44,7 +47,7 @@ namespace Z80toTMS9900console
                 {
                 },
                 new LabelHighlighter());
-            return new TMS9900MultilineTranslator(new Z80LineParser(), singleLineTranslator);
+            return new TMS9900MultilineTranslator(new Z80LineParser(hexPrefix, hexSuffix), singleLineTranslator);
         }
 
         static IEnumerable<string> GetZ80sourceCode(StreamReader reader)
